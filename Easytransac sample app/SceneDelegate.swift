7ebbeafd87de7f150bf5ba065c0d9e77ad44236c
Damien <damien@movidone.com>
2022-03-28 12:09:11 +0200

    Sample app

--- Easytransac sample app/SceneDelegate.swift
+++ Easytransac sample app/SceneDelegate.swift
@@ -10,15 +10,21 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        if let url = URLContexts.first?.url {
+            guard let controller = window?.rootViewController else {
+                return
+            }
 
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+            // callback received when Easytransac is calling the given url scheme
+            print(url.absoluteString)
+            let alert = UIAlertController(title: "Callback received", message: url.absoluteString.removingPercentEncoding, preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            controller.present(alert, animated: true, completion: nil)
+        }
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
@@ -46,7 +52,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
 
--- Easytransac sample app/ViewController.swift
+++ Easytransac sample app/ViewController.swift
@@ -8,12 +8,110 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    static let API_KEY = "YOUR_API_KEY=="
+    static let EASYTRANSAC_SCHEME = "easytransac://pay"
+    static let CALLBACK_SCHEME = "easytransacsampleapp://result";
+    
+    @IBAction func btnFlash(_ sender: Any) {
+        let dict: KeyValuePairs = [
+            "apiKey": ViewController.API_KEY,
+            "paymentMethod": "flash",
+            "amount": "1.07",
+            "isDemo": "true",
+            "callback": ViewController.CALLBACK_SCHEME
+        ];
+        
+        openUrl(generateUrlScheme(dict))
     }
-
-
+    
+    @IBAction func btnManual(_ sender: Any) {
+        let dict: KeyValuePairs = [
+            "apiKey": ViewController.API_KEY,
+            "paymentMethod": "manual",
+            "amount": "1.02",
+            "isDemo": "true",
+            "customerLastname": "Lademo",
+            "customerFirstname": "Paul",
+            "callback": ViewController.CALLBACK_SCHEME
+        ];
+        
+        openUrl(generateUrlScheme(dict))
+    }
+    
+    @IBAction func btnMail(_ sender: Any) {
+        let dict: KeyValuePairs = [
+            "apiKey": ViewController.API_KEY,
+            "paymentMethod": "mail",
+            "amount": "1.03",
+            "isDemo": "true",
+            "customerEmail": "demo@easytransac.com",
+            "customerLastname": "Lademo",
+            "customerFirstname": "Paul",
+            "callback": ViewController.CALLBACK_SCHEME
+        ];
+        
+        openUrl(generateUrlScheme(dict))
+    }
+    
+    @IBAction func btnQRCode(_ sender: Any) {
+        let dict: KeyValuePairs = [
+            "apiKey": ViewController.API_KEY,
+            "paymentMethod": "qrcode",
+            "amount": "1.04",
+            "isDemo": "true",
+            "callback": ViewController.CALLBACK_SCHEME
+        ];
+        
+        openUrl(generateUrlScheme(dict))
+    }
+    
+    @IBAction func btnSms(_ sender: Any) {
+        let dict: KeyValuePairs = [
+            "apiKey": ViewController.API_KEY,
+            "paymentMethod": "sms",
+            "amount": "1.05",
+            "isDemo": "true",
+            "customerCallingCode": "33",
+            "customerPhone": "0611223344",
+            "callback": ViewController.CALLBACK_SCHEME
+        ];
+        
+        openUrl(generateUrlScheme(dict))
+    }
+    
+    @IBAction func actionMultipleFlash(_ sender: Any) {
+        let dict: KeyValuePairs = [
+            "apiKey": ViewController.API_KEY,
+            "paymentMethod": "flash",
+            "amount": "100",
+            "isDemo": "true",
+            "isMultiplePayment": "true",
+            "downPayment": "30",
+            "multiplePaymentRepeat": "3",
+            "callback": ViewController.CALLBACK_SCHEME
+        ];
+        
+        openUrl(generateUrlScheme(dict))
+    }
+    
+    // Generate URL Scheme from given parameters
+    private func generateUrlScheme(_ dict: KeyValuePairs<String, String>) -> String {
+        var url = ViewController.EASYTRANSAC_SCHEME;
+        for values in dict {
+            url.append(url == ViewController.EASYTRANSAC_SCHEME ? "?" : "&");
+            url.append("\(values.key)=\(values.value)")
+        }
+        
+        return url;
+    }
+    
+    private func openUrl(_ urlString: String) {
+        let url = URL(string: urlString)
+        UIApplication.shared.open(url!) { (result) in
+            if result {
+                print("App opened")
+            }
+        }
+    }
 }
-
